85d6fc82453b0370ade67b3a6d32028e40ad60bb
Kamen Vakavchiev <kanitkameh@gmail.com>
2023-10-20 02:57:13 +0300

    reformat on save

---
+++ nvim/ftplugin/scala.lua
@@ -0,0 +1,9 @@
+-- Auto format buffer on save
+vim.api.nvim_create_augroup('AutoFormatting', {})
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.scala',
+  group = 'AutoFormatting',
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})
