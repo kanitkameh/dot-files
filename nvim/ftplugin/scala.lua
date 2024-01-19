049e73abdaeb647ae5e1ca54d13ca9a9d0b4fa0b
Kamen Vakavchiev <kanitkameh@gmail.com>
2024-01-19 17:19:31 +0200

    K to hover in scala worksheet

--- nvim/ftplugin/scala.lua
+++ nvim/ftplugin/scala.lua
@@ -1,3 +1,5 @@
+local map = vim.keymap.set
+
 -- Auto format buffer on save
 vim.api.nvim_create_augroup('AutoFormatting', {})
 vim.api.nvim_create_autocmd('BufWritePre', {
@@ -7,3 +9,5 @@ vim.api.nvim_create_autocmd('BufWritePre', {
     vim.lsp.buf.format()
   end,
 })
+-- Metals worksheet show long lines
+map("n", "<leader>K",  require("metals").hover_worksheet)
