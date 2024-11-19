917207dd5d0a772774217404ba8ea1bbe32b3879
Kamen Vakavchiev <kanitkameh@gmail.com>
2024-11-19 21:14:08 +0200

    add lsp_dynamic_workspace_symbols map to Telescope

--- nvim/lua/plugins/spec1.lua
+++ nvim/lua/plugins/spec1.lua
@@ -246,6 +246,7 @@ return {
     keys = {
       {'<leader>/', '<cmd>Telescope live_grep<cr>', desc = 'Search in Telescope'},
       {'<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Find Files in Telescope'},
+      {'<F4>', '<cmd>Telescope lsp_dynamic_workspace_symbols<cr>', desc = 'Search LSP Symbols in Telescope'},
     }
   },
 
