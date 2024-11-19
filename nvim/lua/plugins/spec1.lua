15e3efad731a7b84d7eb274a8a3225e8648d76d7
Kamen Vakavchiev <kanitkameh@gmail.com>
2024-11-19 19:59:39 +0200

    treesitter highlighting and folding

--- nvim/lua/plugins/spec1.lua
+++ nvim/lua/plugins/spec1.lua
@@ -251,7 +251,21 @@ return {
 
   {
       'nvim-treesitter/nvim-treesitter',
-      build = ':TSUpdate'
+      build = ':TSUpdate',
+      opts = {
+        highlight = {
+          enable = true,
+        },
+      },
+      config = function (_, opts)
+        if type(opts.ensure_installed) == "table" then
+          opts.ensure_installed = LazyVim.dedup(opts.ensure_installed)
+        end
+        require("nvim-treesitter.configs").setup(opts)
+
+        vim.o.foldmethod = 'expr'
+        vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      end
   },
 
   {
