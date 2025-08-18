5d6e3e4d51849cac8f9cc193bd9517173c4778a9
Kamen Vakavchiev <kanitkameh@gmail.com>
2025-08-18 22:51:51 +0300

    use telescope for LSP

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -9,18 +9,10 @@ local map = vim.keymap.set
 vim.opt_global.completeopt = { "menuone", "noinsert", "noselect" }
 
 -- LSP mappings
-map("n", "gd",  vim.lsp.buf.definition, { desc = "Definition"})
-map("n", "gD",  vim.lsp.buf.type_definition, { desc = "Type definition"})
 map("n", "K",  vim.lsp.buf.hover, { desc = "Hover"})
-map("n", "gi", vim.lsp.buf.implementation, { desc = "Implementation"})
-map("n", "gr", vim.lsp.buf.references, { desc = "References"})
-map("n", "gds", vim.lsp.buf.document_symbol, { desc = "Document symbol"})
-map("n", "gws", vim.lsp.buf.workspace_symbol, { desc = "Workspace symbol"})
 map("n", "<leader>cl", vim.lsp.codelens.run, { desc = "Code lens"})
 map("n", "<leader>sh", vim.lsp.buf.signature_help, { desc = "Signature help"})
 map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename"})
-map("n", "gci",  vim.lsp.buf.incoming_calls, { desc = "Incoming Calls"})
-map("n", "gco",  vim.lsp.buf.outgoing_calls, { desc = "Outgoing Calls"})
 map("n", "<leader>=", vim.lsp.buf.format, { desc = "Format"})
 map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action"})
 
@@ -39,9 +31,6 @@ end, { desc = "Workspace errors"})
 map("n", "<leader>aw", function()
     vim.diagnostic.setqflist({ severity = "W" })
 end, { desc = "Workspace warnings"})
-
--- buffer diagnostics only
-map("n", "<leader>bd", vim.diagnostic.setloclist, { desc = "Buffer diagnostics"})
 
 map("n", "[c", function()
     vim.diagnostic.goto_prev({ wrap = false })
